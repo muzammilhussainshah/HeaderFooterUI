596024200934630801be2e12b89cd52ab44848f9
haideralishah <headeralishah@gmail.com>
2020-11-06 18:58:42 +0500

    code finishing

--- src/store/reducer/reducer.js
+++ src/store/reducer/reducer.js
@@ -1,22 +1,5 @@
 import ActionTypes from '../constant/constant';
-var today = new Date;
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-today = yyyy + '-' + mm + '-' + dd;
 const INITIAL_STATE = {
-    isLoader: false,
-    isError: "",
-    thankYou: false,
-    dateChange: today,
-    shops: [],
-    favoriteShops: [],
-    currentUser: [],
-    currentShop: [],
-    bookings: [],
-    currentUserBooking: [],
-    currentBarberBooking: [],
-    availBleSlots: [],
 }
 
 export default (state = INITIAL_STATE, action) => {
@@ -24,62 +7,6 @@ export default (state = INITIAL_STATE, action) => {
         case ActionTypes.SHOPS:
             return ({
                 ...state,
-                shops: action.payload
-            })
-        case ActionTypes.FAVORITESHOPS:
-            return ({
-                ...state,
-                favoriteShops: action.payload
-            })
-        case ActionTypes.CURRENTSHOP:
-            return ({
-                ...state,
-                currentShop: action.payload
-            })
-            case ActionTypes.THANKYOUFORORDER:
-                return ({
-                    ...state,
-                    thankYou: action.payload
-                })
-            case ActionTypes.CURRENTUSER:
-                return ({
-                    ...state,
-                    currentUser: action.payload
-                })
-        case ActionTypes.AVAILBLESLOTS:
-            return ({
-                ...state,
-                availBleSlots: action.payload
-            })
-        case ActionTypes.BOOKINGS:
-            return ({
-                ...state,
-                bookings: action.payload
-            })
-        case ActionTypes.CURRENTUSERBOOKING:
-            return ({
-                ...state,
-                currentUserBooking: action.payload
-            })
-        case ActionTypes.CURRENTBARBERBOOKING:
-            return ({
-                ...state,
-                currentBarberBooking: action.payload
-            })
-        case ActionTypes.ISLOADER:
-            return ({
-                ...state,
-                isLoader: action.payload
-            })
-        case ActionTypes.ISERROR:
-            return ({
-                ...state,
-                isError: action.payload
-            })
-        case ActionTypes.DATECHANGE:
-            return ({
-                ...state,
-                dateChange: action.payload
             })
         default:
             return state;
